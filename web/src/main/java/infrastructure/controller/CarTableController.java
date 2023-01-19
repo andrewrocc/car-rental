79baec81e12c25ef1c57db3620c6923eeaec4af5
andrewrocc <andreyrocc@gmail.com>
2023-01-19 20:17:11 +0300

    get data from REST service and deserialize it

--- web/src/main/java/infrastructure/controller/CarTableController.java
+++ web/src/main/java/infrastructure/controller/CarTableController.java
@@ -1,21 +1,17 @@
 package infrastructure.controller;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.ToNumberPolicy;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
 import infrastructure.dto.CarInfoDTO;
-import infrastructure.service.CarService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.Reader;
-import java.io.StringReader;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -32,15 +28,12 @@ public class CarTableController {
     public ModelAndView getCarTablePage() {
         System.out.println("car table controller: " + now());
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List> entity = restTemplate.getForEntity(URL_REST_SERVICE + "/cars", List.class);
-//        try {
-//            Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting()
-//                    .setNumberToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE).serializeNulls().create();
-//            Object o = gson.fromJson(entity.getBody().toString(), List.class);
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-        return new ModelAndView( "car_table", Map.of("carList", requireNonNull(entity.getBody())));         //carService.getCarTable()  // nonNull(entity.getBody()
+        final String rawData = restTemplate.getForObject(URL_REST_SERVICE + "/cars", String.class);
+        Type type = new TypeToken<List<CarInfoDTO>>() {}.getType();
+        Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting()
+                    .setNumberToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE).serializeNulls().create();
+        List<CarInfoDTO> fromJson = gson.fromJson(rawData, type);
+        return new ModelAndView( "car_table", Map.of("carList", requireNonNull(fromJson)));
     }
 }
 
